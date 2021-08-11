a8fde9388a9b23759ca27ebddeb6783b9801ade4
Vinicius Santiago <viniciussdsilva@gmail.com>
2021-08-10 23:18:06 -0300

    chore: enhance coverage

--- authentication-api/jest.config.js
+++ authentication-api/jest.config.js
@@ -18,6 +18,7 @@ module.exports = {
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
     '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/index.ts'
   ],
 
   // The directory where Jest should output its coverage files
