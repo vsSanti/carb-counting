feb2a3d323e57bcc20a46a0a0714e95d71bc7723
Vinicius Santiago <viniciussdsilva@gmail.com>
2021-08-11 18:21:37 -0300

    feat: ensure PgPatientRepository returns a patient on add success

--- authentication-api/src/infra/db/pg/repositories/pg-patient-repository.ts
+++ authentication-api/src/infra/db/pg/repositories/pg-patient-repository.ts
@@ -1,10 +1,20 @@
 import { getRepository } from 'typeorm';
 
-import { LoadPatientByEmailRepository } from '@/data/protocols/db';
+import { AddPatientRepository, LoadPatientByEmailRepository } from '@/data/protocols/db';
 import { PatientModel } from '@/domain/models';
 import { PgPatient } from '@/infra/db/pg/entities';
+import { AddPatientParams } from '@/domain/usecases';
 
-export class PgPatientRepository implements LoadPatientByEmailRepository {
+export class PgPatientRepository implements AddPatientRepository, LoadPatientByEmailRepository {
+  async add(data: AddPatientParams): Promise<PatientModel> {
+    const pgPatientRepository = getRepository(PgPatient);
+
+    const pgPatient = await pgPatientRepository.save(data);
+    delete pgPatient.password;
+
+    return pgPatient;
+  }
+
   async loadByEmail(email: string): Promise<PatientModel> {
     const pgPatientRepository = getRepository(PgPatient);
     const pgPatient = await pgPatientRepository.findOne({ email });
