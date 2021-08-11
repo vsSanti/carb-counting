66f719dbe90b3869393850c14d7f9d1715e3633f
Vinicius Santiago <viniciussdsilva@gmail.com>
2021-08-11 17:42:05 -0300

    test: ensure PgPatientRepository returns a patient on loadByEmail success

---
+++ authentication-api/tests/infra/db/pg/repositories/pg-patient-repository.spec.ts
@@ -0,0 +1,43 @@
+import { IBackup } from 'pg-mem';
+import { getRepository, Repository, getConnection } from 'typeorm';
+
+import { AddPatientParams } from '@/domain/usecases';
+import { PgPatientRepository } from '@/infra/db/pg/repositories';
+import { PgPatient } from '@/infra/db/pg/entities';
+
+import { mockAddPatientParams } from '@/tests/domain/mocks';
+import { makeFakeDb } from '@/tests/infra/db/pg/mocks';
+
+describe('PgUserAccountRepository', () => {
+  let sut: PgPatientRepository;
+  let pgPatientRepo: Repository<PgPatient>;
+  let addPatientParams: AddPatientParams;
+  let backup: IBackup;
+
+  beforeAll(async () => {
+    const db = await makeFakeDb();
+    backup = db.backup();
+    pgPatientRepo = getRepository(PgPatient);
+  });
+
+  afterAll(async () => {
+    await getConnection().close();
+  });
+
+  beforeEach(() => {
+    backup.restore();
+    sut = new PgPatientRepository();
+    addPatientParams = mockAddPatientParams();
+  });
+
+  describe('load', () => {
+    it('should return a patient if email is already registered', async () => {
+      await pgPatientRepo.save(addPatientParams);
+
+      const patient = await sut.loadByEmail(addPatientParams.email);
+
+      delete addPatientParams.password;
+      expect(patient).toEqual(addPatientParams);
+    });
+  });
+});
