05317591b8513f6c5edf3da3f3188f065b765314
Vinicius Santiago <viniciussdsilva@gmail.com>
2021-08-14 10:52:37 -0300

    test: ensure EmailValidator returns an error string if param isn't an email

--- authentication-api/tests/validation/validators/string-validation/email-validator.spec.ts
+++ authentication-api/tests/validation/validators/string-validation/email-validator.spec.ts
@@ -11,4 +11,11 @@ describe('Email Validator', () => {
     const errorMessage = sut.validate({ fieldName: 'email', input: { email: 'any@email.com' } });
     expect(errorMessage).toBeUndefined();
   });
+
+  it("should return an error string if parameter isn't a an email", () => {
+    const errorMessage = sut.validate({ fieldName: 'email', input: { email: 'invalid_email' } });
+    expect(errorMessage).toBeTruthy();
+    expect(typeof errorMessage).toBe('string');
+    expect(errorMessage).toBe("It isn't a valid email.");
+  });
 });
