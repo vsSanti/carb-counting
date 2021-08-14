5aaf3217a5007b852c2b5987b67a7310c621ecf9
Vinicius Santiago <viniciussdsilva@gmail.com>
2021-08-14 15:38:45 -0300

    test: ensure SignUpController returns 400 if ObjectValidator returns an error

--- authentication-api/tests/presentation/controllers/login/signup-controller.spec.ts
+++ authentication-api/tests/presentation/controllers/login/signup-controller.spec.ts
@@ -1,4 +1,5 @@
 import { SignUpController } from '@/presentation/controllers/login';
+import { badRequest } from '@/presentation/helpers/http/http-helper';
 import { HttpRequest } from '@/presentation/protocols';
 
 import { mockAddPatientParams } from '@/tests/domain/mocks';
@@ -17,15 +18,23 @@ const mockRequest = (): HttpRequest => {
 describe('SignUp Controller', () => {
   let objectValidatorSpy: ObjectValidatorSpy;
   let sut: SignUpController;
+  let httpRequest: HttpRequest;
 
   beforeEach(() => {
     objectValidatorSpy = new ObjectValidatorSpy();
     sut = new SignUpController(objectValidatorSpy);
+    httpRequest = mockRequest();
   });
 
   it('should call ObjectValidator with correct params', async () => {
-    const httpRequest = mockRequest();
     await sut.handle(httpRequest);
     expect(objectValidatorSpy.params).toEqual({ input: httpRequest.body });
+  });
+
+  it('should return 400 if ObjectValidator returns an error', async () => {
+    objectValidatorSpy.response = { hasErrors: true, errors: { field: ['error'] } };
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse).toEqual(badRequest({ validationErrors: { field: ['error'] } }));
   });
 });
