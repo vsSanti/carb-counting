bd7582a2bb734812c58c6dfb9d72f1604b35deda
Vinicius Santiago <viniciussdsilva@gmail.com>
2021-08-14 10:44:57 -0300

    test: ensure StringValidator returns an error string if param isn't a string

--- authentication-api/tests/validation/validators/string-validation/string-validator.spec.ts
+++ authentication-api/tests/validation/validators/string-validation/string-validator.spec.ts
@@ -11,4 +11,11 @@ describe('String Validator', () => {
     const errorMessage = sut.validate({ fieldName: 'field', input: { field: 'valid_string' } });
     expect(errorMessage).toBeUndefined();
   });
+
+  it("should return an error string if parameter isn't a string", () => {
+    const errorMessage = sut.validate({ fieldName: 'field', input: { field: 10 } });
+    expect(errorMessage).toBeTruthy();
+    expect(typeof errorMessage).toBe('string');
+    expect(errorMessage).toBe("It isn't a string.");
+  });
 });
