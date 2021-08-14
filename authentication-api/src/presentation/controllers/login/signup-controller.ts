bfa1e70ca5fb0c1117c849b430038f6614375213
Vinicius Santiago <viniciussdsilva@gmail.com>
2021-08-14 16:09:01 -0300

    feat: ensure SignUpController calls AddPatient with correct params

--- authentication-api/src/presentation/controllers/login/signup-controller.ts
+++ authentication-api/src/presentation/controllers/login/signup-controller.ts
@@ -1,14 +1,44 @@
+import { AddPatient } from '@/domain/usecases';
 import { badRequest } from '@/presentation/helpers/http/http-helper';
 import { Controller, HttpRequest, HttpResponse, ObjectValidator } from '@/presentation/protocols';
 
 export class SignUpController implements Controller {
-  constructor(private readonly validation: ObjectValidator) {}
+  constructor(
+    private readonly validation: ObjectValidator,
+    private readonly addPatient: AddPatient
+  ) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const validation = this.validation.validate({ input: httpRequest.body });
+    const { body } = httpRequest;
+
+    const validation = this.validation.validate({ input: body });
     if (validation.hasErrors) {
       return badRequest({ validationErrors: validation.errors });
     }
+
+    const {
+      email,
+      password,
+      name,
+      sex,
+      height,
+      glycemicTarget,
+      insulinUnitsPerDay,
+      weight,
+      birthDate,
+    } = body;
+
+    await this.addPatient.add({
+      email,
+      password,
+      name,
+      sex,
+      height,
+      glycemicTarget,
+      insulinUnitsPerDay,
+      weight,
+      birthDate,
+    });
     return;
   }
 }
