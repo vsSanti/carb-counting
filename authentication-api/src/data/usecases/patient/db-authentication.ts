402bb6fd689fd2e92bc7741fc9a7a47f32eb18d3
Vinicius Santiago <viniciussdsilva@gmail.com>
2021-08-14 17:09:52 -0300

    feat: ensure DbAuthentication calls LoadPatientByEmailRepository with correct email

---
+++ authentication-api/src/data/usecases/patient/db-authentication.ts
@@ -0,0 +1,13 @@
+import { LoadPatientByEmailRepository } from '@/data/protocols/db';
+import { AuthenticationModel } from '@/domain/models';
+import { Authentication, AuthenticationParams } from '@/domain/usecases';
+
+export class DbAuthentication implements Authentication {
+  constructor(private readonly loadPatientByEmailRepository: LoadPatientByEmailRepository) {}
+
+  async auth(params: AuthenticationParams): Promise<AuthenticationModel> {
+    const { email } = params;
+    await this.loadPatientByEmailRepository.loadByEmail(email);
+    return;
+  }
+}
--- authentication-api/src/domain/usecases/index.ts
+++ authentication-api/src/domain/usecases/index.ts
@@ -1,1 +1,2 @@
 export * from './add-patient';
+export * from './authentication';
