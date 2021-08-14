16d7ba35efeefd12bd1ec44056945bb2cb316ee4
Vinicius Santiago <viniciussdsilva@gmail.com>
2021-08-14 18:59:46 -0300

    test: ensure JwtAdapter throws if sign throws

--- authentication-api/tests/infra/criptography/jwt-adapter.spec.ts
+++ authentication-api/tests/infra/criptography/jwt-adapter.spec.ts
@@ -2,6 +2,8 @@ import jwt from 'jsonwebtoken';
 
 import { JwtAdapter } from '@/infra/criptography';
 
+import { throwError } from '@/tests/domain/mocks';
+
 jest.mock('jsonwebtoken', () => ({
   async sign(): Promise<string> {
     return 'token';
@@ -25,6 +27,12 @@ describe('Jwt Adapter', () => {
     it('should return a token on sign success', async () => {
       const accessToken = await sut.encrypt('any_id');
       expect(accessToken).toBe('token');
+    });
+
+    it('should throw if sign throws', async () => {
+      jest.spyOn(jwt, 'sign').mockImplementationOnce(throwError);
+      const promise = sut.encrypt('any_id');
+      await expect(promise).rejects.toThrow();
     });
   });
 });
