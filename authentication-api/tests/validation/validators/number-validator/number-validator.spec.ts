a9d040716afa4bbcfd933a85b58db15c28ccd27c
Vinicius Santiago <viniciussdsilva@gmail.com>
2021-08-15 10:38:48 -0300

    test: ensure NumberValidator returns undefined if parameter is a number

--- authentication-api/tests/validation/validators/number-validator/number-validator.spec.ts
+++ authentication-api/tests/validation/validators/number-validator/number-validator.spec.ts
@@ -7,6 +7,11 @@ describe('Number Validator', () => {
     sut = new NumberValidator();
   });
 
+  it('should return undefined if parameter is a number', () => {
+    const errorMessage = sut.validate({ fieldName: 'field', input: { field: 0 } });
+    expect(errorMessage).toBeUndefined();
+  });
+
   it("should return an error string if parameter isn't a number", () => {
     const errorMessage = sut.validate({ fieldName: 'field', input: { field: 'string' } });
     expect(errorMessage).toBeTruthy();
