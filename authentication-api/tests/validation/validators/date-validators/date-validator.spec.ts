35214a3484ac1885eaa94546cd8e2519412ff20b
Vinicius Santiago <viniciussdsilva@gmail.com>
2021-08-15 11:20:14 -0300

    test: ensure DateValidator returns an error string if param isn't a valid date

--- authentication-api/tests/validation/validators/date-validators/date-validator.spec.ts
+++ authentication-api/tests/validation/validators/date-validators/date-validator.spec.ts
@@ -1,6 +1,6 @@
 import { DateValidator } from '@/validation/validators/date-validators';
 
-describe('Number Validator', () => {
+describe('Date Validator', () => {
   let sut: DateValidator;
 
   beforeEach(() => {
@@ -13,5 +13,12 @@ describe('Number Validator', () => {
       input: { field: new Date().toISOString() },
     });
     expect(errorMessage).toBeUndefined();
+  });
+
+  it("should return an error string if parameter isn't a string", () => {
+    const errorMessage = sut.validate({ fieldName: 'field', input: { field: 'invalid_date' } });
+    expect(errorMessage).toBeTruthy();
+    expect(typeof errorMessage).toBe('string');
+    expect(errorMessage).toBe("It isn't a date.");
   });
 });
