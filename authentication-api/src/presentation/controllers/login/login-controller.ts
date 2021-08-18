d088e559c8cc6973303bb3ac7a988560ed373529
Vinicius Santiago <viniciussdsilva@gmail.com>
2021-08-18 19:07:37 -0300

    feat: ensure LoginController returns 400 if ObjectValidator returns an error

--- authentication-api/src/presentation/controllers/login/login-controller.ts
+++ authentication-api/src/presentation/controllers/login/login-controller.ts
@@ -1,3 +1,4 @@
+import { badRequest } from '@/presentation/helpers/http/http-helper';
 import { Controller, HttpRequest, HttpResponse, ObjectValidator } from '@/presentation/protocols';
 
 export class LoginController implements Controller {
@@ -6,7 +7,10 @@ export class LoginController implements Controller {
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     const { body } = httpRequest;
 
-    this.validation.validate({ input: body });
+    const validation = this.validation.validate({ input: body });
+    if (validation.hasErrors) {
+      return badRequest({ validationErrors: validation.errors });
+    }
 
     return null;
   }
