8300b50f50395eec46179c9383a148f4fb8f69a8
Vinicius Santiago <viniciussdsilva@gmail.com>
2021-08-18 22:27:59 -0300

    feat: ensure RefreshTokensController calls LoadPatientByToken with correct value

--- authentication-api/src/presentation/controllers/login/refresh-tokens-controller.ts
+++ authentication-api/src/presentation/controllers/login/refresh-tokens-controller.ts
@@ -1,8 +1,12 @@
+import { LoadPatientByToken } from '@/domain/usecases';
 import { badRequest } from '@/presentation/helpers/http/http-helper';
 import { Controller, HttpRequest, HttpResponse, ObjectValidator } from '@/presentation/protocols';
 
 export class RefreshTokensController implements Controller {
-  constructor(private readonly validation: ObjectValidator) {}
+  constructor(
+    private readonly validation: ObjectValidator,
+    private readonly loadPatientByToken: LoadPatientByToken
+  ) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     const { body } = httpRequest;
@@ -11,6 +15,10 @@ export class RefreshTokensController implements Controller {
     if (validation.hasErrors) {
       return badRequest({ validationErrors: validation.errors });
     }
+
+    const { refreshToken } = body;
+
+    await this.loadPatientByToken.load(refreshToken);
 
     return null;
   }
