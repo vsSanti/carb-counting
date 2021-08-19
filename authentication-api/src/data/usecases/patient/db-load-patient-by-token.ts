5cd8c7361a2dde5ad2fa9c4060835daad9d5e5ab
Vinicius Santiago <viniciussdsilva@gmail.com>
2021-08-18 21:58:19 -0300

    feat: ensure DbLoadPatientByToken calls LoadPatientByIdRepository with correct value

--- authentication-api/src/data/usecases/patient/db-load-patient-by-token.ts
+++ authentication-api/src/data/usecases/patient/db-load-patient-by-token.ts
@@ -1,16 +1,23 @@
 import { Decrypter } from '@/data/protocols/criptography';
+import { LoadPatientByIdRepository } from '@/data/protocols/db';
 import { PatientModel } from '@/domain/models';
 import { LoadPatientByToken } from '@/domain/usecases';
 
 export class DbLoadPatientByToken implements LoadPatientByToken {
-  constructor(private readonly decrypter: Decrypter) {}
+  constructor(
+    private readonly decrypter: Decrypter,
+    private readonly loadPatientByIdRepository: LoadPatientByIdRepository
+  ) {}
 
   async load(token: string): Promise<PatientModel> {
+    let id: string;
     try {
-      await this.decrypter.decrypt(token);
+      id = await this.decrypter.decrypt(token);
     } catch (error) {
       return null;
     }
+
+    await this.loadPatientByIdRepository.loadById(id);
 
     return null;
   }
