e7c52aa5241a16ddb74eb8512b303be0e479420d
Vinicius Santiago <viniciussdsilva@gmail.com>
2021-08-19 21:11:10 -0300

    feat: add makeAuthorizerController

---
+++ authentication-api/src/main/factories/controllers/login/authorizer/authorizer-controller-factory.ts
@@ -0,0 +1,9 @@
+import { makeDbLoadPatientByToken } from '@/main/factories/usecases/patient/db-load-patient-by-token';
+import { AuthorizerController } from '@/presentation/controllers/login';
+import { Controller } from '@/presentation/protocols';
+
+export const makeAuthorizerController = (): Controller => {
+  const refreshTokensController = new AuthorizerController(makeDbLoadPatientByToken());
+
+  return refreshTokensController;
+};
