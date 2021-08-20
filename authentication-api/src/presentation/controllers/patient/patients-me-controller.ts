c8cebb6a9a301ad24c2cc4e73f72e40877e21387
Vinicius Santiago <viniciussdsilva@gmail.com>
2021-08-19 22:53:23 -0300

    refactor: remove ObjectValidation from PatientsMeController

--- authentication-api/src/presentation/controllers/patient/patients-me-controller.ts
+++ authentication-api/src/presentation/controllers/patient/patients-me-controller.ts
@@ -1,21 +1,13 @@
 import { LoadPatientById } from '@/domain/usecases';
-import { badRequest, ok, serverError, unauthorized } from '@/presentation/helpers/http/http-helper';
-import { Controller, HttpRequest, HttpResponse, ObjectValidator } from '@/presentation/protocols';
+import { ok, serverError, unauthorized } from '@/presentation/helpers/http/http-helper';
+import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols';
 
 export class PatientsMeController implements Controller {
-  constructor(
-    private readonly validation: ObjectValidator,
-    private readonly loadPatientById: LoadPatientById
-  ) {}
+  constructor(private readonly loadPatientById: LoadPatientById) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const { patientId } = httpRequest;
-
-      const validation = this.validation.validate({ input: { patientId } });
-      if (validation.hasErrors) {
-        return badRequest({ validationErrors: validation.errors });
-      }
 
       const patient = await this.loadPatientById.load(patientId);
       if (!patient) return unauthorized();
--- authentication-api/tests/presentation/controllers/patient/patients-me-controller.spec.ts
+++ authentication-api/tests/presentation/controllers/patient/patients-me-controller.spec.ts
@@ -2,10 +2,9 @@ import faker from 'faker';
 
 import { PatientsMeController } from '@/presentation/controllers/patient';
 import { ServerError } from '@/presentation/errors';
-import { badRequest, ok, serverError, unauthorized } from '@/presentation/helpers/http/http-helper';
+import { ok, serverError, unauthorized } from '@/presentation/helpers/http/http-helper';
 import { HttpRequest } from '@/presentation/protocols';
 
-import { ObjectValidatorSpy } from '@/tests/validation/mocks';
 import { throwError } from '@/tests/domain/mocks';
 import { LoadPatientByIdSpy } from '@/tests/presentation/mocks';
 
@@ -14,30 +13,16 @@ const mockRequest = (patientId: string): HttpRequest => {
 };
 
 describe('PatientsMe Controller', () => {
-  let objectValidatorSpy: ObjectValidatorSpy;
   let loadPatientByIdSpy: LoadPatientByIdSpy;
   let sut: PatientsMeController;
   let httpRequest: HttpRequest;
   let patientId: string;
 
   beforeEach(() => {
-    objectValidatorSpy = new ObjectValidatorSpy();
     loadPatientByIdSpy = new LoadPatientByIdSpy();
-    sut = new PatientsMeController(objectValidatorSpy, loadPatientByIdSpy);
+    sut = new PatientsMeController(loadPatientByIdSpy);
     patientId = faker.datatype.uuid();
     httpRequest = mockRequest(patientId);
-  });
-
-  it('should call ObjectValidator with correct params', async () => {
-    await sut.handle(httpRequest);
-    expect(objectValidatorSpy.params).toEqual({ input: httpRequest });
-  });
-
-  it('should return 400 if ObjectValidator returns an error', async () => {
-    objectValidatorSpy.response = { hasErrors: true, errors: { field: ['error'] } };
-    const httpResponse = await sut.handle(httpRequest);
-    expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse).toEqual(badRequest({ validationErrors: { field: ['error'] } }));
   });
 
   it('should call LoadPatientById with correct value', async () => {
