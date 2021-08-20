d933bb8b26c43302d93f875507b9175a32b70495
Vinicius Santiago <viniciussdsilva@gmail.com>
2021-08-19 22:51:23 -0300

    refactor: change lambdaRouteAdapter to recieve more methods and also private endpoints

--- authentication-api/src/main/adapters/lambda-route-adapter.ts
+++ authentication-api/src/main/adapters/lambda-route-adapter.ts
@@ -2,32 +2,37 @@ import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
 import { APIGatewayEvent, ProxyResult } from 'aws-lambda';
 import { openTypeORMConnection } from '../helper/open-typeorm-connection';
 
-export type LambdaRouteAdapterParams = {
-  post: {
-    handler: Controller;
-  };
+type EventHandler = {
+  isPrivate?: boolean;
+  handler: Controller;
 };
 
-export const lambdaRouteAdapter = ({ post }: LambdaRouteAdapterParams) => {
+export type LambdaRouteAdapterParams = {
+  get?: EventHandler;
+  post?: EventHandler;
+};
+
+export const lambdaRouteAdapter = (params: LambdaRouteAdapterParams) => {
   return async (event: APIGatewayEvent): Promise<ProxyResult> => {
     await openTypeORMConnection();
 
-    const method = event.httpMethod;
+    const method: EventHandler = params[event.httpMethod.toLowerCase()];
+    if (!method) {
+      return {
+        statusCode: 405,
+        body: JSON.stringify({ errorMessage: 'Method Not Allowed' }),
+      };
+    }
+
     const httpRequest: HttpRequest = {
       body: JSON.parse(event.body),
     };
 
-    let httpResponse: HttpResponse;
-    if (post && method === 'POST') {
-      httpResponse = await post.handler.handle(httpRequest);
-    } else {
-      httpResponse = {
-        statusCode: 405,
-        body: {
-          errorMessage: 'Method Not Allowed',
-        },
-      };
+    if (method.isPrivate) {
+      httpRequest.patientId = event.requestContext.authorizer.principalId;
     }
+
+    const httpResponse: HttpResponse = await method.handler.handle(httpRequest);
 
     return {
       statusCode: httpResponse.statusCode,
