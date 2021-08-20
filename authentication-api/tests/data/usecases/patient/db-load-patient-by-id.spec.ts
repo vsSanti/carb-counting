dc46ee46f5616fb129f9ab6fc8ca74a23ec55c09
Vinicius Santiago <viniciussdsilva@gmail.com>
2021-08-19 21:39:39 -0300

    test: ensure DbLoadPatientById throws if LoadPatientByIdRepository throws

--- authentication-api/tests/data/usecases/patient/db-load-patient-by-id.spec.ts
+++ authentication-api/tests/data/usecases/patient/db-load-patient-by-id.spec.ts
@@ -3,6 +3,7 @@ import faker from 'faker';
 import { DbLoadPatientById } from '@/data/usecases/patient/db-load-patient-by-id';
 
 import { LoadPatientByIdRepositorySpy } from '@/tests/data/mocks';
+import { throwError } from '@/tests/domain/mocks';
 
 describe('DbLoadPatientById Usecase', () => {
   let loadPatientByIdRepositorySpy: LoadPatientByIdRepositorySpy;
@@ -24,5 +25,11 @@ describe('DbLoadPatientById Usecase', () => {
     loadPatientByIdRepositorySpy.patientModel = null;
     const account = await sut.load(id);
     expect(account).toBeNull();
+  });
+
+  it('should throw if LoadPatientByIdRepository throws', async () => {
+    jest.spyOn(loadPatientByIdRepositorySpy, 'loadById').mockImplementationOnce(throwError);
+    const errorPromise = sut.load(id);
+    await expect(errorPromise).rejects.toThrow();
   });
 });
