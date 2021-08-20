8ff467eb78970507b7e10e78c1435b07d304adda
Vinicius Santiago <viniciussdsilva@gmail.com>
2021-08-19 21:58:16 -0300

    test: ensure PatientsMeController returns 401 if no patient is found on LoadPatientById

--- authentication-api/tests/presentation/controllers/patient/patients-me-controller.spec.ts
+++ authentication-api/tests/presentation/controllers/patient/patients-me-controller.spec.ts
@@ -1,6 +1,7 @@
 import faker from 'faker';
 
 import { PatientsMeController } from '@/presentation/controllers/patient';
+import { unauthorized } from '@/presentation/helpers/http/http-helper';
 import { HttpRequest } from '@/presentation/protocols';
 
 import { LoadPatientByIdSpy } from '@/tests/presentation/mocks';
@@ -22,8 +23,14 @@ describe('PatientsMe Controller', () => {
     httpRequest = mockRequest(id);
   });
 
-  it('should call LoadPatientByToken with correct value', async () => {
+  it('should call LoadPatientById with correct value', async () => {
     await sut.handle(httpRequest);
     expect(loadPatientByIdSpy.id).toEqual(httpRequest.patientId);
+  });
+
+  it('should return 401 if no patient is found on LoadPatientById', async () => {
+    loadPatientByIdSpy.patientModel = null;
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse).toEqual(unauthorized());
   });
 });
