63d0a9a7d67b82a921b20f84568246c8ab11ad24
Vinicius Santiago <viniciussdsilva@gmail.com>
2021-08-22 12:04:10 -0300

    feat: add ListFoods spy

---
+++ carb-counting-api/tests/presentation/mocks/mock-food.ts
@@ -0,0 +1,14 @@
+import { FoodModel } from '@/domain/models';
+import { ListFoods } from '@/domain/usecases';
+
+import { mockFoodModelList } from '@/tests/domain/mocks';
+
+export class ListFoodsSpy implements ListFoods {
+  patientModel = mockFoodModelList();
+  timesCalled = 0;
+
+  async list(): Promise<FoodModel[]> {
+    this.timesCalled += 1;
+    return Promise.resolve(this.patientModel);
+  }
+}
