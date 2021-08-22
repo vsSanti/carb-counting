7d94d60ea0cbf0ceaf33c54b6cbce3d901a22a1e
Vinicius Santiago <viniciussdsilva@gmail.com>
2021-08-22 17:20:12 -0300

    feat: ensure DbListFoods calls ListFoodsRepository once

---
+++ carb-counting-api/src/data/usecases/meal/db-list-foods.ts
@@ -0,0 +1,12 @@
+import { ListFoodsRepository } from '@/data/protocols/db';
+import { FoodModel } from '@/domain/models';
+import { ListFoods } from '@/domain/usecases';
+
+export class DbListFoods implements ListFoods {
+  constructor(private readonly listFoodsRepository: ListFoodsRepository) {}
+
+  async list(): Promise<FoodModel[]> {
+    await this.listFoodsRepository.listAll();
+    return null;
+  }
+}
