cb5633ee2ea837238c3603a5b17c3950a164b863
Vinicius Santiago <viniciussdsilva@gmail.com>
2021-08-22 17:21:34 -0300

    test: ensure DbListFoods throws if ListFoodsRepository throws

--- carb-counting-api/tests/data/usecases/db-list-foods.spec.ts
+++ carb-counting-api/tests/data/usecases/db-list-foods.spec.ts
@@ -1,5 +1,7 @@
 import { DbListFoods } from '@/data/usecases/meal/db-list-foods';
+
 import { ListFoodsRepositorySpy } from '@/tests/data/mocks';
+import { throwError } from '@/tests/domain/mocks';
 
 describe('DbListFoods Usecase', () => {
   let listFoodsRepositorySpy: ListFoodsRepositorySpy;
@@ -13,5 +15,11 @@ describe('DbListFoods Usecase', () => {
   it('should call ListFoodsRepository once', async () => {
     await sut.list();
     expect(listFoodsRepositorySpy.timesCalled).toBe(1);
+  });
+
+  it('should throw if ListFoodsRepository throws', async () => {
+    jest.spyOn(listFoodsRepositorySpy, 'listAll').mockImplementationOnce(throwError);
+    const promise = sut.list();
+    await expect(promise).rejects.toThrow();
   });
 });
