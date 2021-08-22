ee92ece5b299e86af97b1625435b339a7d48fee3
Vinicius Santiago <viniciussdsilva@gmail.com>
2021-08-22 19:36:03 -0300

    feat: add openTypeORMConnection helper

---
+++ meal-api/src/main/helper/open-typeorm-connection.ts
@@ -0,0 +1,34 @@
+import { createConnection, getConnection, ConnectionOptions } from 'typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+
+import env from '@/main/config/env';
+import { PgFood } from '@/infra/db/pg/entities';
+
+export async function openTypeORMConnection(): Promise<any> {
+  const options: ConnectionOptions = {
+    name: 'default',
+    type: 'postgres',
+    port: 5432,
+    synchronize: false,
+    logging: false,
+    host: env.database.host,
+    database: env.database.database,
+    username: env.database.username,
+    password: env.database.password,
+    entities: [PgFood],
+    namingStrategy: new SnakeNamingStrategy(),
+    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
+  };
+
+  try {
+    if (process.env.NODE_ENV === 'production') {
+      return getConnection(options.name);
+    }
+
+    await getConnection(options.name).close();
+    return createConnection(options);
+  } catch (error) {
+    console.log('[DB] Creating a new DB connection.');
+    return createConnection(options);
+  }
+}
