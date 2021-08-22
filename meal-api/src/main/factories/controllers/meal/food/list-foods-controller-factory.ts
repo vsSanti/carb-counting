e99df4f74f90fcebe0e015bec65dd50df73b2b42
Vinicius Santiago <viniciussdsilva@gmail.com>
2021-08-22 19:32:24 -0300

    refactor: change factories directory

--- meal-api/src/main/factories/controllers/meal/food/list-foods-controller-factory.ts
+++ meal-api/src/main/factories/controllers/meal/food/list-foods-controller-factory.ts
@@ -1,6 +1,7 @@
-import { makeDbListFoods } from '@/main/usecases/meal/db-list-foods-factory';
-import { ListFoodsController } from '@/presentation/controllers/meal';
 import { Controller } from 'presentation-common';
+
+import { makeDbListFoods } from '@/main/factories/usecases/meal/db-list-foods-factory';
+import { ListFoodsController } from '@/presentation/controllers/meal';
 
 export const makeListFoodsController = (): Controller => {
   const listFoodsController = new ListFoodsController(makeDbListFoods());
