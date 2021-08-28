fa66463227880c2a0de8ca297d96ec5e148d3083
Vinicius Santiago <viniciussdsilva@gmail.com>
2021-08-28 18:49:53 -0300

    fix: database connection

--- apis/src/common/main/config/env.ts
+++ apis/src/common/main/config/env.ts
@@ -1,9 +1,9 @@
 export default {
-  jwtSecret: process.env.JWT_SECRET || 'otijas818SSD(*-0)))@,dasoip',
+  jwtSecret: process.env.LOCAL_JWT_SECRET ?? process.env.JWT_SECRET,
   database: {
-    host: process.env.DB_HOST ?? 'localhost',
-    database: process.env.DB_DATABASE_NAME ?? 'carb-counting-auth-local',
-    username: process.env.DB_USERNAME ?? 'postgres',
-    password: process.env.DB_PASSWORD ?? 'docker',
+    host: process.env.LOCAL_DB_HOST ?? process.env.DB_HOST,
+    database: process.env.LOCAL_DB_DATABASE_NAME ?? process.env.DB_DATABASE_NAME,
+    username: process.env.LOCAL_DB_USERNAME ?? process.env.DB_USERNAME,
+    password: process.env.LOCAL_DB_PASSWORD ?? process.env.DB_PASSWORD,
   },
 };
