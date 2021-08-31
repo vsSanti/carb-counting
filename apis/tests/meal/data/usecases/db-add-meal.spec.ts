86d52c42c0b679ee2cce76b072d7eeb1ba954f8a
Vinicius Santiago <viniciussdsilva@gmail.com>
2021-08-30 21:17:23 -0300

    test: ensure DbAddMeal throws if AddMealRepository throws

--- apis/tests/meal/data/usecases/db-add-meal.spec.ts
+++ apis/tests/meal/data/usecases/db-add-meal.spec.ts
@@ -1,6 +1,7 @@
 import { DbAddMeal } from '@/meal/data/usecases/meal/db-add-meal';
 import { AddMealParams } from '@/meal/domain/usecases';
 
+import { throwError } from '@/tests/common/domain';
 import { AddMealRepositorySpy } from '@/tests/meal/data/mocks';
 import { mockAddMealParams } from '@/tests/meal/domain/mocks';
 
@@ -18,5 +19,11 @@ describe('DbAddMeal Usecase', () => {
   it('should call AddMealRepository with correct params', async () => {
     await sut.add(addMealParams);
     expect(addMealRepositorySpy.params).toEqual(addMealParams);
+  });
+
+  it('should throw if AddMealRepository throws', async () => {
+    jest.spyOn(addMealRepositorySpy, 'add').mockImplementationOnce(throwError);
+    const errorPromise = sut.add(addMealParams);
+    await expect(errorPromise).rejects.toThrow();
   });
 });
