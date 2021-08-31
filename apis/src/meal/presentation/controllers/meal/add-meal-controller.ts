deaa0ba3a5c74adc8eb39051492dcbb8e0113e9c
Vinicius Santiago <viniciussdsilva@gmail.com>
2021-08-31 17:49:04 -0300

    feat: ensure AddMealController calls AddMeal with correct params

--- apis/src/meal/presentation/controllers/meal/add-meal-controller.ts
+++ apis/src/meal/presentation/controllers/meal/add-meal-controller.ts
@@ -1,14 +1,21 @@
 import { badRequest } from '@/common/presentation/helpers';
 import { Controller, HttpRequest, HttpResponse } from '@/common/presentation/protocols';
 import { ObjectValidator } from '@/common/validation/protocols';
+import { AddMeal } from '@/meal/domain/usecases';
 
 export class AddMealController implements Controller {
-  constructor(private readonly validation: ObjectValidator) {}
+  constructor(private readonly validation: ObjectValidator, private readonly addMeal: AddMeal) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     const { body } = httpRequest;
 
     const validation = this.validation.validate({ input: body });
-    return badRequest({ validationErrors: validation.errors });
+    if (validation.hasErrors) {
+      return badRequest({ validationErrors: validation.errors });
+    }
+
+    await this.addMeal.add(body);
+
+    return null;
   }
 }
