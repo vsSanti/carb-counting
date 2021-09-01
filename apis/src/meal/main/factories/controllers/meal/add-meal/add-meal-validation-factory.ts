65910d6ffeb73788427dba1b32c50e9956add972
Vinicius Santiago <viniciussdsilva@gmail.com>
2021-08-31 23:46:08 -0300

    feat: add AddMealValidation factory

---
+++ apis/src/meal/main/factories/controllers/meal/add-meal/add-meal-validation-factory.ts
@@ -0,0 +1,47 @@
+import {
+  GenericObjectArrayPropertyBuilder,
+  GenericObjectValidator,
+  GenericPropertyBuilder,
+} from '@/common/validation/builders';
+import { ObjectValidator } from '@/common/validation/protocols';
+import { ArrayValidator, NumberValidator, StringValidator } from '@/common/validation/validators';
+
+export const makeAddMealValidation = (): ObjectValidator => {
+  const stringValidator = new StringValidator();
+  const numberValidator = new NumberValidator();
+
+  const patientIdProperty = new GenericPropertyBuilder('patientId', [stringValidator]);
+  const patientInsulinUnitsPerDayProperty = new GenericPropertyBuilder(
+    'patientInsulinUnitsPerDay',
+    [numberValidator]
+  );
+  const patientGlycemicTargetProperty = new GenericPropertyBuilder('patientGlycemicTarget', [
+    numberValidator,
+  ]);
+  const glucoseMeasurementProperty = new GenericPropertyBuilder('glucoseMeasurement', [
+    numberValidator,
+  ]);
+
+  const mealFoodsWeightProperty = new GenericPropertyBuilder('weight', [numberValidator]);
+  const mealFoodsFoodIdProperty = new GenericPropertyBuilder('foodId', [stringValidator]);
+  const mealFoodsSchema = new GenericObjectValidator([
+    mealFoodsWeightProperty,
+    mealFoodsFoodIdProperty,
+  ]);
+
+  const mealFoodsProperty = new GenericObjectArrayPropertyBuilder(
+    'mealFoods',
+    new ArrayValidator({ validLength: true }),
+    mealFoodsSchema
+  );
+
+  const objectValidator = new GenericObjectValidator([
+    patientIdProperty,
+    patientInsulinUnitsPerDayProperty,
+    patientGlycemicTargetProperty,
+    glucoseMeasurementProperty,
+    mealFoodsProperty,
+  ]);
+
+  return objectValidator;
+};
