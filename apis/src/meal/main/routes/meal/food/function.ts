e4f6d494a8d882af183215172edb738784008ff7
Vinicius Santiago <viniciussdsilva@gmail.com>
2021-08-31 23:24:29 -0300

    refactor: change file directory

--- apis/src/meal/main/routes/meal/food/function.ts
+++ apis/src/meal/main/routes/meal/food/function.ts
@@ -1,7 +1,7 @@
 import 'source-map-support/register';
 
 import { lambdaRouteAdapter } from '@/common/main/adapters/lambda-route-adapter';
-import { makeListFoodsController } from '@/meal/main/factories/controllers/meal/list-foods-controller-factory';
+import { makeListFoodsController } from '@/meal/main/factories/controllers/meal/list-foods/list-foods-controller-factory';
 import { typeORMMealOptions } from '@/common/main/helper/typeorm-options';
 
 export const handler = lambdaRouteAdapter({
