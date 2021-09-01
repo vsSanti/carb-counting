98b23bd529a43924b8ca35c744af48af022a6e4c
Vinicius Santiago <viniciussdsilva@gmail.com>
2021-08-31 21:02:03 -0300

    feat: ensure ArrayValidator returns an error string if array is empty

--- apis/src/common/validation/validators/array-validators/array-validator.ts
+++ apis/src/common/validation/validators/array-validators/array-validator.ts
@@ -1,11 +1,22 @@
 import { PropertyValidation, PropertyValidationParams } from '@/common/validation/protocols';
 
+type ArrayValidatorOptions = {
+  validLength?: boolean;
+};
+
 export class ArrayValidator implements PropertyValidation {
+  constructor(private readonly options?: ArrayValidatorOptions) {}
+
   validate({ fieldName, input }: PropertyValidationParams): string {
     const values = input[fieldName];
 
     if (!Array.isArray(values)) {
       return "It isn't an array.";
     }
+
+    if (!this.options) return;
+
+    const { validLength = true } = this.options;
+    if (validLength && !values.length) return 'Array must contain at least one record.';
   }
 }
