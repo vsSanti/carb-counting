62993f076fb2c0575d4dbc5095620df180ee9259
Vinicius Santiago <viniciussdsilva@gmail.com>
2021-09-02 00:06:02 -0300

    feat: add GET meals/{mealId} route

--- apis/src/meal/main/routes/meal/meal/function.ts
+++ apis/src/meal/main/routes/meal/meal/function.ts
@@ -2,10 +2,14 @@ import 'source-map-support/register';
 
 import { lambdaRouteAdapter } from '@/common/main/adapters/lambda-route-adapter';
 import { makeAddMealController } from '@/meal/main/factories/controllers/meal/add-meal/add-meal-controller-factory';
+import { makeLoadMealByIdController } from '@/meal/main/factories/controllers/meal/load-meal-by-id/load-meal-by-id-controller-factory';
 
 export const handler = lambdaRouteAdapter({
   dbName: 'meal',
   isPrivate: true,
+  get: {
+    handler: makeLoadMealByIdController(),
+  },
   post: {
     handler: makeAddMealController(),
   },
