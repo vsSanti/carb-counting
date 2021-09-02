f3cd85ce74ea6273f8d805072d0b0f3dfd5bd2c1
Vinicius Santiago <viniciussdsilva@gmail.com>
2021-09-01 23:33:05 -0300

    feat: ensure LoadMealByIdController returns 500 if LoadMealById throws

--- apis/src/meal/presentation/controllers/meal/load-meal-by-id-controller.ts
+++ apis/src/meal/presentation/controllers/meal/load-meal-by-id-controller.ts
@@ -1,4 +1,4 @@
-import { notFound } from '@/common/presentation/helpers';
+import { notFound, serverError } from '@/common/presentation/helpers';
 import { Controller, HttpRequest, HttpResponse } from '@/common/presentation/protocols';
 import { LoadMealById } from '@/meal/domain/usecases';
 
@@ -6,10 +6,15 @@ export class LoadMealByIdController implements Controller {
   constructor(private readonly loadMealById: LoadMealById) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { mealId } = httpRequest.pathParameters;
+    try {
+      const { mealId } = httpRequest.pathParameters;
 
-    await this.loadMealById.load(mealId);
+      await this.loadMealById.load(mealId);
 
-    return notFound('meal');
+      return notFound('meal');
+    } catch (error) {
+      console.error(error);
+      return serverError(error);
+    }
   }
 }
