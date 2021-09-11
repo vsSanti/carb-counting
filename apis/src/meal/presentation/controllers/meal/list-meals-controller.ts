c5bc76b4334c3012689bd128edc2302cf3ce935f
Vinicius Santiago <viniciussdsilva@gmail.com>
2021-09-11 16:59:54 -0300

    feat: ensure ListMealsController returns 500 if ListMeals throws

--- apis/src/meal/presentation/controllers/meal/list-meals-controller.ts
+++ apis/src/meal/presentation/controllers/meal/list-meals-controller.ts
@@ -1,14 +1,20 @@
 import { ListMeals } from '@/meal/domain/usecases';
+import { serverError } from '@/common/presentation/helpers';
 import { Controller, HttpRequest, HttpResponse } from '@/common/presentation/protocols';
 
 export class ListMealsController implements Controller {
   constructor(private readonly listMeals: ListMeals) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { page } = httpRequest.queryStringParameters;
+    try {
+      const { page } = httpRequest.queryStringParameters;
 
-    await this.listMeals.list({ page });
+      await this.listMeals.list({ page });
 
-    return null;
+      return null;
+    } catch (error) {
+      console.error(error);
+      return serverError(error);
+    }
   }
 }
