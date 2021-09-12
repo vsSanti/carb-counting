380cc17226be7fad40d7452f2b0112092fda52d2
Vinicius Santiago <viniciussdsilva@gmail.com>
2021-09-11 21:12:15 -0300

    feat: add list meals route

--- apis/src/meal/main/routes/meal/meal/function.ts
+++ apis/src/meal/main/routes/meal/meal/function.ts
@@ -3,12 +3,17 @@ import 'source-map-support/register';
 import { lambdaRouteAdapter } from '@/common/main/adapters/lambda-route-adapter';
 import { makeAddMealController } from '@/meal/main/factories/controllers/meal/add-meal/add-meal-controller-factory';
 import { makeLoadMealByIdController } from '@/meal/main/factories/controllers/meal/load-meal-by-id/load-meal-by-id-controller-factory';
+import { makeListMealsController } from '@/meal/main/factories/controllers/meal/list-meals/list-foods-controller-factory';
 
 export const handler = lambdaRouteAdapter({
   dbName: 'meal',
   isPrivate: true,
   get: {
+    handler: makeListMealsController(),
+  },
+  getById: {
     handler: makeLoadMealByIdController(),
+    resource: '/meals/{mealId}',
   },
   post: {
     handler: makeAddMealController(),
