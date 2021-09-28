4f24f1959b5aab558ad38f1c24c7a115766fb3d5
Vinicius Santiago <viniciussdsilva@gmail.com>
2021-09-27 21:12:26 -0300

    chore: add styled-components

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,19 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import { StatusBar } from 'react-native';
+import Home from './src/screens/Home';
 
 export default function App(): React.ReactElement {
   return (
-    <View style={styles.container}>
-      <Text>Aplicativo mobile iniciado!</Text>
-    </View>
+    <>
+      <StatusBar barStyle="light-content" />
+      <Home />
+    </>
   );
 }
