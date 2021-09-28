f9231a6f5df4d761cf81e999baeddd00f9e84e2c
Vinicius Santiago <viniciussdsilva@gmail.com>
2021-09-28 08:52:02 -0300

    chore: finish styles configuration

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,12 +1,33 @@
+/* eslint-disable camelcase */
 import React from 'react';
-import { StatusBar } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { ThemeProvider } from 'styled-components';
+
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from '@expo-google-fonts/poppins';
+
+import theme from './src/global/styles/theme';
+
 import Home from './src/screens/Home';
 
 export default function App(): React.ReactElement {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
-    <>
-      <StatusBar barStyle="light-content" />
+    <ThemeProvider theme={theme}>
       <Home />
-    </>
+    </ThemeProvider>
   );
 }
