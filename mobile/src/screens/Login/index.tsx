f299adcef5c52d48dce519594f1775fbca71a9c9
Vinicius Santiago <viniciussdsilva@gmail.com>
2021-10-03 15:07:14 -0300

    feat: finish Login screen layout

--- mobile/src/screens/Login/index.tsx
+++ mobile/src/screens/Login/index.tsx
@@ -1,5 +1,16 @@
-import React from 'react';
-import { Keyboard, TouchableWithoutFeedback } from 'react-native';
+import React, { useCallback } from 'react';
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableWithoutFeedback,
+} from 'react-native';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+
+import { Button } from '@/components/Button';
+import { InputForm } from '@/components/Form/InputForm';
 
 import {
   Container,
@@ -10,25 +21,80 @@ import {
   TitleWrapper,
   Title,
   SignInTitle,
+  Footer,
+  FooterWrapper,
 } from './styles';
 
+type FormData = {
+  email: string;
+  password: string;
+};
+
+const schema = yup.object().shape({
+  email: yup.string().email('E-mail precisa ser válido').required('Nome é obrigatório'),
+  password: yup.string().required('Senha é obrigatório'),
+});
+
 export const Login: React.FC = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const handleLogin = useCallback((data: FormData) => {
+    console.log(data);
+  }, []);
+
   return (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <Container>
-        <Header>
-          <TitleWrapper>
-            <LogoWrapper>
-              <LogoIcon name="calculator" />
-              <LogoTitle>CarbCounting</LogoTitle>
-            </LogoWrapper>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      style={{ flex: 1 }}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <Container>
+          <Header>
+            <TitleWrapper>
+              <LogoWrapper>
+                <LogoIcon name="calculator" />
+                <LogoTitle>CarbCounting</LogoTitle>
+              </LogoWrapper>
 
-            <Title>Sua contagem de carboidratos {'\n'} de forma simples</Title>
-          </TitleWrapper>
+              <Title>Sua contagem de carboidratos {'\n'} de forma simples</Title>
+            </TitleWrapper>
 
-          <SignInTitle>Faça seu login em sua conta</SignInTitle>
-        </Header>
-      </Container>
-    </TouchableWithoutFeedback>
+            <SignInTitle>Faça seu login em sua conta</SignInTitle>
+          </Header>
+
+          <Footer>
+            <FooterWrapper>
+              <InputForm
+                name="email"
+                control={control}
+                placeholder="E-mail"
+                keyboardType="email-address"
+                autoCapitalize="none"
+                autoCorrect={false}
+                error={errors.email?.message}
+              />
+
+              <InputForm
+                name="password"
+                control={control}
+                placeholder="Senha"
+                autoCapitalize="none"
+                autoCorrect={false}
+                secureTextEntry
+                error={errors.password?.message}
+              />
+
+              <Button title="Entrar" onPress={handleSubmit(handleLogin)} />
+            </FooterWrapper>
+          </Footer>
+        </Container>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 };
--- mobile/src/screens/Login/styles.tsx
+++ mobile/src/screens/Login/styles.tsx
@@ -1,14 +1,15 @@
 import styled from 'styled-components/native';
-import { RFValue } from 'react-native-responsive-fontsize';
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
 import { SimpleLineIcons } from '@expo/vector-icons';
 
 export const Container = styled.View`
-  flex: 1;
+  /* flex: 1; */
+  justify-content: flex-end;
 `;
 
 export const Header = styled.View`
   width: 100%;
-  height: 70%;
+  height: 60%;
 
   background-color: ${({ theme }) => theme.colors.primary};
 
@@ -42,7 +43,6 @@ export const SignInTitle = styled.Text`
 `;
 
 export const LogoWrapper = styled.View`
-  /* flex-direction: row; */
   align-items: center;
 `;
 
@@ -57,4 +57,18 @@ export const LogoTitle = styled.Text`
   font-size: ${RFValue(30)}px;
 
   margin-top: ${RFValue(15)}px;
+`;
+
+export const Footer = styled.View`
+  width: 100%;
+  height: 40%;
+
+  /* background-color: ${({ theme }) => theme.colors.secondary}; */
+`;
+
+export const FooterWrapper = styled.View`
+  margin-top: ${RFPercentage(-4)}px;
+  padding: 0 32px;
+
+  justify-content: space-between;
 `;
